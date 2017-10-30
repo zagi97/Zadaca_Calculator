b32df5c14100d2678be339a4a4d2ecf96d03277e
Kruno <Kruno@KRUNO-PC>
2017-10-30 14:42:24 +0100

    Zadaca

---
+++ ConsoleAppTimer/ConsoleAppTimer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            ScheduleService();
+            Console.ReadKey();
+
+        }
+        public static void ScheduleService()
+            
+            {
+            // Objekt klase Timer 
+            Timer Schedular = new Timer(new TimerCallback(SchedularCallback));
+
+            // Postavljanje vremena 'po defaultu'
+            DateTime scheduledTime = DateTime.MinValue;
+
+            int intervalMinutes = 1;
+
+            // Postavljanje vremena zapisa u trenutno vrijeme + 1 minuta
+            scheduledTime = DateTime.Now.AddMinutes(intervalMinutes);
+            if (DateTime.Now > scheduledTime)
+            {
+                scheduledTime = scheduledTime.AddMinutes(intervalMinutes);
+            }
+
+            // Vremenski interval
+            TimeSpan timeSpan = scheduledTime.Subtract(DateTime.Now);
+            string schedule = string.Format("{0} day(s) {1} hour(s) {2} minute(s) {3} seconds(s)", timeSpan.Days, timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+            WriteToFile("Simple Service scheduled to run after: " + schedule + " {0}");
+
+            //Razlika između trenutnog vremena i planiranog vremena
+            int dueTime = Convert.ToInt32(timeSpan.TotalMilliseconds);
+
+            // Promjena vremena izvršavanja metode povratnog poziva.
+            Schedular.Change(dueTime, Timeout.Infinite);
+        }
+        private static void SchedularCallback(object e)
+        {
+            WriteToFile("Simple Service Log: {0}");
+            ScheduleService();
+        }
+        private static void WriteToFile(string text)
+        {
+            string path = "E:\\ServiceLog.txt";
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine(string.Format(text, DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+                writer.Close();
+            }
+        }
+
+    }
+}
+
---
+++ ConsoleAppTimer/WindowsLoggingService/Service1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsLoggingService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public string sDateTime;
+        public string sText;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            sDateTime = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss");
+            sText = "Servis je pokrenut" + sDateTime;
+            WriteToFile(sText);
+        }
+
+        protected override void OnStop()
+        {
+            sDateTime = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss");
+            sText = "Servis je zaustavljen" + sDateTime;
+            WriteToFile(sText);
+        }
+        private static void WriteToFile(string text)
+        {
+            string path = "E:\\ServiceLog.txt";
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine(string.Format(text, DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+                writer.Close();
+            }
+        }
+
+    }
+}
